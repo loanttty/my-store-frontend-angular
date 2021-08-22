77b7f2a4306515c4840673fa27e57b674cf9b528
LoanTT <79430764+loanttty@users.noreply.github.com>
2021-08-22 15:30:15 +0800

    Initial set-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { ProductConfirmationComponent } from './components/product-confirmation/product-confirmation.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    ProductItemDetailComponent,
+    ProductConfirmationComponent,
+    ConfirmationComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
